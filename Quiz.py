5f27d8f50c4275a73b62695b099d68ab398c563e
hanas879 <hanas879@milo.life>
2018-11-05 11:42:45 +0100

    OS clear
    Added the os library to clear the terminal screen after each question

--- Quiz.py
+++ Quiz.py
@@ -1,5 +1,6 @@
 import random
 from time import sleep
+import os
 
 questions = ["What is A?", "What is B?", "What is C?", "What is D?", "What is E?", "What is F?", "What is G?", "What is H?", "What is I?", "What is J?", "What is K?", "What is L?", "What is M?", "What is N?", "What is O?", "What is P?", "What is Q?", "What is R?", "What is S?", "What is T?", "What is U?", "What is V?", "What is W?", "What is X?", "What is Y?", "What is Z?"]
 answers = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
@@ -7,6 +8,9 @@ stopCommands = ["exit", "Exit", "quit", "Quit", "stop", "Stop"]
 quit = False
 
 score = 0
+
+def clear():
+	os.system("cls" if os.name=="nt" else "clear")
 
 while quit == False:
 	
@@ -22,6 +26,7 @@ while quit == False:
 		print("CORRECT! Your score is:", score)
 		print("########################################")
 		sleep(3)
+		clear()
 
 	elif answer in stopCommands:
 		quit = True
@@ -34,5 +39,6 @@ while quit == False:
 		print("Your score has been reset. It is now: ", score)
 		print("########################################")
 		sleep(6)
+		clear()
 
 print("Goodbye!")
